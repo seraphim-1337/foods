a56737b5d8db65a4706531dab043ce4e96656e4c
seraphim-1337 <senseylitvinov@gmail.com>
2026-05-20 13:08:23 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const slides = document.querySelectorAll('.offer__slide'),
+      prev = document.querySelector('.offer__slider-prev'),
+      next = document.querySelector('.offer__slider-next'),
+      currentNum = document.querySelector('#current'),
+      totalNum = document.querySelector('#total');
+
+
+let slideIndex = 0; 
+
+if (slides.length < 10) {
+    totalNum.textContent = `0${slides.length}`;
+} else {
+    totalNum.textContent = slides.length;
+}
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        slideIndex = 0;
+    }
+    if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove('show', 'fade');
+        slide.classList.add('hide'); 
+    });
+
+    slides[slideIndex].classList.add('show', 'fade');
+    slides[slideIndex].classList.remove('hide');
+
+
+    if (slideIndex + 1 < 10) {
+        currentNum.textContent = `0${slideIndex + 1}`;
+    } else {
+        currentNum.textContent = slideIndex + 1;
+    }
+}
+
+showSlide(slideIndex);
+
+next.onclick = () => {
+    slideIndex++;
+    showSlide(slideIndex);
+};
+
+prev.onclick = () => {
+    slideIndex--;
+    showSlide(slideIndex);
+};
+
